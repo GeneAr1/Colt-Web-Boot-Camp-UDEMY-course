e0dbf9a88b5f8dd68f5a01aba6c05c76925f3dbe
Gene Arsenault <geneaar@outlook.com>
2018-02-26 13:53:44 -0500

    Incremental Commit
    Added change color function in Js to change all squares when correct color picked
    
    added logick for correct and wrong picks for main game
    
    started working on the game text boxes

--- My Color Game/colorgm.js
+++ My Color Game/colorgm.js
@@ -1,7 +1,7 @@
 // alert("connected");
 
 // set up array of the 6 colors
-var  colors = [
+var colors = [
     "rgb(255, 0, 0)",
     "rgb(255, 255, 0)",
     "rgb(0, 255, 0)",
@@ -14,17 +14,20 @@ var  colors = [
 // we can randomize first get all the squares out ot the document
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];    // will randomize later
+var pickedColor = colors[3]; // will randomize later
+
+messageDisplay = document.getElementById("message");
 
 var colorDisplay = document.querySelector("#colorDisplay"); // get the picked color to show up on the web page
 colorDisplay.textContent = pickedColor;
 
-for (var i = 0; i < squares.length; i++){
+
+for (var i = 0; i < squares.length; i++) {
     // add initial colors to the squares
     squares[i].style.backgroundColor = colors[i];
-    
+
     // add a click event listner to the squres
-    squares[i].addEventListener("click", function() {
+    squares[i].addEventListener("click", function () {
 
         // alert("clicked a square") // comment out this line after check
         //cgrab the color of the square 
@@ -33,13 +36,23 @@ for (var i = 0; i < squares.length; i++){
         //compare color to picked color
 
         if (clickedColor === pickedColor) {
-            alert("correct");
+            // alert("correct");
+            messageDisplay.textContent = "correct";
+            ChangeColors(clickedColor);
         } else {
-            alert("wrong");
+            // alert("wrong"); 
+            //you want to fade the color square if wrong incorrect
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
         }
-        
 
     });
+}
 
-
+function ChangeColors (color){
+    // loop throught the squares and change the color to the correct color or pickedColor
+    for (var i = 0; i < squares.length; i++) {
+        // change all squares to the correct color
+        squares[i].style.backgroundColor = color;
+    }
 }
