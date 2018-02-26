87a4fce2b8aec4ad27a0601134a0fa616393a006
Gene Arsenault <geneaar@outlook.com>
2018-02-26 11:47:00 -0500

    Incremental commit no randomizagion yet
    hard code logic for checking and comparing color and if correct color

--- My Color Game/colorgm.js
+++ My Color Game/colorgm.js
@@ -14,8 +14,32 @@ var  colors = [
 // we can randomize first get all the squares out ot the document
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];    // will randomize later
+
+var colorDisplay = document.querySelector("#colorDisplay"); // get the picked color to show up on the web page
+colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++){
+    // add initial colors to the squares
+    squares[i].style.backgroundColor = colors[i];
+    
+    // add a click event listner to the squres
+    squares[i].addEventListener("click", function() {
 
-    squares[i].style.background = colors[i];
+        // alert("clicked a square") // comment out this line after check
+        //cgrab the color of the square 
+        //alert(this.style.backgroundColor);  // do not forget the style to access the color just declare a variable
+        var clickedColor = this.style.backgroundColor;
+        //compare color to picked color
+
+        if (clickedColor === pickedColor) {
+            alert("correct");
+        } else {
+            alert("wrong");
+        }
+        
+
+    });
+
+
 }
