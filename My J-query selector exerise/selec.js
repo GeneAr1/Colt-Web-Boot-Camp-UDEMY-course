1b1fc623a9b726ff6e6c24735c86ccb1dbb6457c
Gene Arsenault <geneaar@gmail.com>
2018-03-01 12:17:30 -0500

    commit JQuery selector exercise files

---
+++ My J-query selector exerise/selec.js
@@ -0,0 +1,13 @@
+// alert("connect")
+
+// select all div give purpe backgound
+$("div").css("background","purple");
+
+// select eht dives with class "highlight" and mak them 200 px wide
+$(".highlight").css("width", "200px");
+
+// select the dive with id "third" and give it a orange border
+$("#third").css("border", "3px solid orange" );
+
+// select first dive only and give pink font
+$("div").first().css("color","pink");
