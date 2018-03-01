b0245b02eeb4a60da4e6bff6ae04f855994336e1
Gene Arsenault <geneaar@gmail.com>
2018-03-01 12:24:34 -0500

    added comment for clarity
    added a comment for clarithy on the first of type function

--- My J-query selector exerise/selec.js
+++ My J-query selector exerise/selec.js
@@ -10,4 +10,7 @@ $(".highlight").css("width", "200px");
 $("#third").css("border", "3px solid orange" );
 
 // select first dive only and give pink font
+// could use div:first or div:first-of-type. first-of-type is fastest because it is built
+// in CSS selsector while the others are JS functions and need to run so they are a little
+//slower
 $("div").first().css("color","pink");
