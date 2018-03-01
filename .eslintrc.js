bea13eda377f50c8386e777ebfd35a628ae887c7
Gene Arsenault <geneaar@gmail.com>
2018-03-01 17:11:59 -0500

    incremental commit of files
    HTML skeleton
    todo selector start in js
    minor style in css

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,20 @@ module.exports = {
     "env": {
         "browser": true,
         "commonjs": true,
-        "es6": true
+        "es6": true,
+        "node": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
         "sourceType": "module"
     },
     "rules": {
         "indent": [
             "error",
-            "tab"
+            4
         ],
         "linebreak-style": [
             "error",
--- My ToDo list Course Project/assets/js/toDo.js
+++ My ToDo list Course Project/assets/js/toDo.js
@@ -1,1 +1,21 @@
-alert("connected");
+//turn of specific todos by clicking
+
+$("li").click(function () {
+
+    // if li is gray
+    // need to use rgb to compare css.("color") returns rgb
+    if ($(this).css("color") === "rgb(128, 128, 128)") {
+        //turn it black
+        $(this).css("color", "black");
+        $(this).css("text-decoration", "none");
+        //else turn it gray
+    } else {
+        // $(this).css("color", "gray");
+        // $(this).css("text-decoration", "line-through");
+        //  or declare an object in css less typing
+        $(this).css({
+            color: "gray",
+            textDecoration: "line-through"
+        });
+    }
+});
