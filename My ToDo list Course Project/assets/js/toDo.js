24b7690912e0a3c430ea0c333d483bbd01e7d6f8
Gene Arsenault <geneaar@gmail.com>
2018-03-01 23:23:18 -0500

    commit todo.js and css
    toDo.css  - added a complete class to use for keeping track of finished items
    
    toDo.js - added jQuery code for selecting and completing items using a class toggle and left original code in plance

--- My ToDo list Course Project/assets/js/toDo.js
+++ My ToDo list Course Project/assets/js/toDo.js
@@ -2,20 +2,35 @@
 
 $("li").click(function () {
 
-    // if li is gray
-    // need to use rgb to compare css.("color") returns rgb
-    if ($(this).css("color") === "rgb(128, 128, 128)") {
-        //turn it black
-        $(this).css("color", "black");
-        $(this).css("text-decoration", "none");
-        //else turn it gray
-    } else {
-        // $(this).css("color", "gray");
-        // $(this).css("text-decoration", "line-through");
-        //  or declare an object in css less typing
-        $(this).css({
-            color: "gray",
-            textDecoration: "line-through"
-        });
-    }
+    // this line replaces all the lines below by toggleing the class
+    //using jQuery it and using css class it will keep track of change and
+    // change color and line through
+    //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+    $(this).toggleClass("completed");
+    //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+    // // if li is gray
+    // // need to use rgb to compare css.("color") returns rgb
+    // if ($(this).css("color") === "rgb(128, 128, 128)") {
+    //     //turn it black
+    //     $(this).css("color", "black");
+    //     $(this).css("text-decoration", "none");
+    //     //else turn it gray
+    // } else {
+    //     // $(this).css("color", "gray");
+    //     // $(this).css("text-decoration", "line-through");
+    //     //  or declare an object in css less typing
+    //     $(this).css({
+    //         color: "gray",
+    //         textDecoration: "line-through"
+    //     });
+    // }
+});
+//click on X to delete todo
+$("span").click(function (event) {
+    $(this).parent().fadeOut(700, function () {
+        //call remove on .this no need for parent already there
+        $(this).remove();
+    });
+    // $(this).parent().remove();
+    event.stopPropagation(); //stp[s event bubbling up
 });
